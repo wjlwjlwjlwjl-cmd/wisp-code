d8a9c7f64fe7ba97a5821df57f98a8154f9a8489
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-08-30 10:43:26 +0800

    删除不必要的表

--- deploy/test/app/mysql/init/init.sql
+++ deploy/test/app/mysql/init/init.sql
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
 source /opt/sql/inituser.sql;
 source /opt/sql/nacos.sql;
-source /opt/sql/db.sql;
-=======
-source /opt/sql/inituser.sql;
-source /opt/sql/nacos.sql;
-source /opt/sql/db.sql;
->>>>>>> Stashed changes
 source /opt/sql/nacosdata.sql;
