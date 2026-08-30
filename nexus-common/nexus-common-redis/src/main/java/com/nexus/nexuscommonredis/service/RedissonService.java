d4a42170b9f14d0ba2450235403886af5945661c
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-08-30 04:49:28 +0000

    解决nacos无法找到 redis 配置问题，正在解决docker网络问题

--- nexus-common/nexus-common-redis/src/main/java/com/nexus/nexuscommonredis/service/RedissonService.java
+++ nexus-common/nexus-common-redis/src/main/java/com/nexus/nexuscommonredis/service/RedissonService.java
@@ -5,18 +5,14 @@ import java.util.concurrent.TimeUnit;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@SuppressWarnings({"rawtypes", "null"})
 public class RedissonService {
-    @Autowired
-    RedisTemplate redisTemplate;
-
     /**
      * Redis 操作客户端
      */
+    @Autowired
     private RedissonClient redissonClient;
 
     /**
