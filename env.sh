c8d5700e650ddc62eff647e461186e56ef984755
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-08-30 23:09:41 +0800

    dashscope

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 cat >> ~/.bashrc << EOF
-export DASHSCOPE_API_KEY=sk-a5f277ca38f8426ba23c68a313c6c46b
+export DASHSCOPE_API_KEY=sk-ws-H.EXPILDL.yOBh.MEYCIQCSzzIgH7_mnxAEqWZWLJLTk6T3ofWfSSbqVDrT0GMr8gIhAK6ynmp-ZF7VhsxmVxPh9qXqnpWujuRyhG6t6_o7qoEn
 export NACOS_ADDR=http://127.0.0.1:8848;
 export RUN_ENV=test
 EOF
