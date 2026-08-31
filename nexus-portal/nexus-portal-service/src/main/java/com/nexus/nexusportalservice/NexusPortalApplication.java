405876d6464a1bc52a5e15d9d77b0a313062c855
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-08-31 19:52:59 +0800

    portal 部署

--- nexus-portal/nexus-portal-service/src/main/java/com/nexus/nexusportalservice/NexusPortalApplication.java
+++ nexus-portal/nexus-portal-service/src/main/java/com/nexus/nexusportalservice/NexusPortalApplication.java
@@ -1,11 +1,14 @@
 package com.nexus.nexusportalservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class NexusPortalApplication {
     public static void main(String[] args){
         SpringApplication.run(NexusPortalApplication.class, args);
+        log.info("wisp-code start successfully");
     }
 }
