938b0e51c53b0199f7fdf0d839542b91450eb443
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-09-05 08:39:29 +0800

    修改 mysql 部署

--- deploy/test/app/mysql/sql/app.sql
+++ deploy/test/app/mysql/sql/app.sql
@@ -6,8 +6,10 @@ CREATE TABLE `app` (
                        `app_name` varchar(10) NOT NULL COMMENT '应用名称',
                        `app_desc` varchar(100) NOT NULL COMMENT '应用描述',
                        `app_doc` text DEFAULT NULL COMMENT '应用需求文档',
+                       `preview_url` varchar(100) NOT NULL COMMENT '应用预览url'
                        `app_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '应用类型：0=html，1=vue3，2=vue3_spring',
                        `app_screenshot` varchar(100) DEFAULT NULL COMMENT '应用截图',
                        PRIMARY KEY (`id`),
                        KEY `idx_apps_user` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000001 DEFAULT CHARSET=utf8mb4 COMMENT='应用信息表'
+GRANT ALL PRIVILEGES ON wispcode.* TO  'bitedev'@'%';
