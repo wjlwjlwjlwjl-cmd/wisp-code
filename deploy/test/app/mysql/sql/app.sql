c6f3f589da05f4f20fdc4a7d22d11cd9735b2c03
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-09-05 00:29:50 +0000

    更新 wispcode 部署文档

--- deploy/test/app/mysql/sql/app.sql
+++ deploy/test/app/mysql/sql/app.sql
@@ -6,6 +6,7 @@ CREATE TABLE `app` (
                        `app_name` varchar(10) NOT NULL COMMENT '应用名称',
                        `app_desc` varchar(100) NOT NULL COMMENT '应用描述',
                        `app_doc` text DEFAULT NULL COMMENT '应用需求文档',
+		       `preview_url` varchar(100) NOT NULL COMMENT '应用预览地址',
                        `app_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '应用类型：0=html，1=vue3，2=vue3_spring',
                        `app_screenshot` varchar(100) DEFAULT NULL COMMENT '应用截图',
                        PRIMARY KEY (`id`),
