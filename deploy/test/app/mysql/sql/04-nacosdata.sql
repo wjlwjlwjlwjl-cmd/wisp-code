a0294cd97e7a85775ea1ae60aa19e21059ed0c84
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-09-06 13:16:12 +0800

    docker 预览阶段 nacos és 数据配置

--- deploy/test/app/mysql/sql/04-nacosdata.sql
+++ deploy/test/app/mysql/sql/04-nacosdata.sql
@@ -3,8 +3,7 @@
 
 use `frameworkjava_nacos_test`;
 INSERT INTO config_info (data_id,group_id,content,md5,gmt_create,gmt_modified,src_user,src_ip,app_name,tenant_id,c_desc,c_use,effect,`type`,c_schema,encrypted_data_key) VALUES
-('share-common-test.yaml','DEFAULT_GROUP','# feign 配置
-feign:
+('share-common-test.yaml','DEFAULT_GROUP','feign:
   okhttp:
     enabled: true
   httpclient:
@@ -18,7 +17,14 @@ feign:
     request:
       enabled: true
     response:
-      enabled: true','7a98e5666bfc562d278db12ea4cae5ed',now(),now(),'nacos','112.46.64.96','通用公共配置','frameworkjava-test','','','','yaml','',''),
+      enabled: true
+app:
+  preview:
+    container-name: wispcode-userapp-preview
+  host: 192.168.160.131
+docker:
+  host: tcp://192.168.160.131:2376
+  cert: /workspace/cert','6572759c52633434823509b70b430c4e',now(),now(),'nacos','112.46.64.96','通用公共配置','frameworkjava-test','','','','yaml','',''),
 
 ('share-redis-test.yaml','DEFAULT_GROUP','spring:
   cache:
@@ -32,41 +38,29 @@ feign:
     host: frameworkjava-redis
     port: 6379
     password: bite@123
-','7102665c408544966342724b5213051c',now(),now(),'nacos','172.19.0.1','通用Redis公共配置','frameworkjava-test','','','','yaml','',''),
+','15803074b36c38b09331395091643875',now(),now(),'nacos','172.19.0.1','通用Redis公共配置','frameworkjava-test','','','','yaml','',''),
 
 ('share-mysql-test.yaml','DEFAULT_GROUP','spring:
   datasource:
-    url: jdbc:mysql://192.168.160.131:3306/frameworkjava_test?useSSL=false&autoReconnect=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT%2B8
+    url: jdbc:mysql://192.168.160.131:3306/wispcode?useSSL=false&autoReconnect=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT%2B8
     driver-class-name: com.mysql.cj.jdbc.Driver
     username: bitedev
     password: bite@123
-    # 指定为HikariDataSource
     type: com.zaxxer.hikari.HikariDataSource
-    # hikari连接池配置
     hikari:
-      #连接池名
       pool-name: HikariCP
-      #最小空闲连接数
       minimum-idle: 5
-      # 空闲连接存活最大时间，默认10分钟
       idle-timeout: 600000
-      # 连接池最大连接数，默认是10
       maximum-pool-size: 10
-      # 此属性控制从池返回的连接的默认自动提交行为,默认值：true
       auto-commit: true
-      # 此属性控制池中连接的最长生命周期，值0表示无限生命周期，默认30分钟
       max-lifetime: 1800000
-      # 数据库连接超时时间,默认30秒
       connection-timeout: 30000
-      # 连接测试query
       connection-test-query: SELECT 1
 mybatis-plus:
-    # 搜索指定包别名
     typeAliasesPackage: com.bitejiuyeke.**.domain
-    # 配置mapper的扫描，找到所有的mapper.xml映射文件
     mapperLocations: classpath*:mapper/**.xml
     configuration:
-      log-impl: org.apache.ibatis.logging.stdout.StdOutImpl','0517d425d111e174fe3f2b799acbd3b1',now(),now(),'nacos','172.18.0.1','通用mysql公共配置','frameworkjava-test','','','','yaml','',''),
+      log-impl: org.apache.ibatis.logging.stdout.StdOutImpl','2256b897837515b4253601b87468809e',now(),now(),'nacos','172.18.0.1','通用mysql公共配置','frameworkjava-test','','','','yaml','',''),
 
 ('bite-gateway-test.yaml','DEFAULT_GROUP','spring:
   cloud:
@@ -119,14 +113,15 @@ security:
       - /**/login/**
       - /**/send_code/**
       - /**/nologin/**
-      - /**/test/**','043148345f3f04a6d79a10daff04c0c3',now(),now(),'nacos','112.46.64.96','网关','frameworkjava-test','','','','yaml','',''),
+      - /**/test/**','a0519254280c43363872c546441b4655',now(),now(),'nacos','112.46.64.96','网关','frameworkjava-test','','','','yaml','',''),
+
 ('share-rabbitmq-test.yaml','DEFAULT_GROUP','spring:
   rabbitmq:
     port: 5672
     host: 192.168.160.131
     virtual-host: /
     username: admin
-    password: bite@123','12b937d4c3b995bd0fa22f1eb03bc51f',now(),now(),'nacos','112.46.64.96','通用rabbitmq公共配置','frameworkjava-test','','','','yaml','','');
+    password: bite@123','3b76b88c362b16b73256b7715248c78c',now(),now(),'nacos','112.46.64.96','通用rabbitmq公共配置','frameworkjava-test','','','','yaml','','');
 
 
 INSERT INTO config_info (data_id,group_id,content,md5,gmt_create,gmt_modified,src_user,src_ip,app_name,tenant_id,c_desc,c_use,effect,`type`,c_schema,encrypted_data_key) VALUES
@@ -134,7 +129,7 @@ INSERT INTO config_info (data_id,group_id,content,md5,gmt_create,gmt_modified,sr
   build:
     initial-capacity: 128
     maximum-size: 1024
-    expire: 60','2257485195db60455f57aa575950a2ff',now(),now(),'nacos','112.46.64.96','本地缓存公共配置','frameworkjava-test',NULL,NULL,NULL,'yaml',NULL,'');
+    expire: 60','6304904002990117226212343872707c',now(),now(),'nacos','112.46.64.96','本地缓存公共配置','frameworkjava-test',NULL,NULL,NULL,'yaml',NULL,'');
 
 
 INSERT INTO tenant_info (kp,tenant_id,tenant_name,tenant_desc,create_source,gmt_create,gmt_modified) VALUES
