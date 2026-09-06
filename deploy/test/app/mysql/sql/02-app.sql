9550ff7d377c23787b110eaf8cb3c971975f3634
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-09-06 13:02:27 +0800

    spring_vue测试通过

--- deploy/test/app/mysql/sql/02-app.sql
+++ deploy/test/app/mysql/sql/02-app.sql
@@ -3,7 +3,7 @@ USE wispcode;
 CREATE TABLE `app` (
                        `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '应用主键ID',
                        `user_id` bigint NOT NULL COMMENT '所属用户主键ID',
-                       `app_name` varchar(10) DEFAULT NULL COMMENT '应用名称',
+                       `app_name` varchar(100) DEFAULT NULL COMMENT '应用名称',
                        `app_desc` varchar(100) DEFAULT NULL COMMENT '应用描述',
                        `app_doc` text DEFAULT NULL COMMENT '应用需求文档',
                        `preview_url` varchar(100) DEFAULT NULL COMMENT '应用预览url',
