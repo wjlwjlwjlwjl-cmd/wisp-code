315ffbb25f9830c86c6b2391ce0e26e33b023811
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-09-16 23:23:04 +0800

    修改表字段

--- deploy/test/app/mysql/sql/02-app.sql
+++ deploy/test/app/mysql/sql/02-app.sql
@@ -2,12 +2,12 @@ CREATE DATABASE IF NOT EXISTS wispcode default character set utf8mb4 collate utf
 USE wispcode;
 CREATE TABLE `app` (
                        `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '应用主键ID',
-                       `user_id` varchar(100) NOT NULL COMMENT '所属用户主键ID',
+                       `user_id` varchar(100) DEFAULT NULL COMMENT '所属用户主键ID',
                        `app_name` varchar(100) DEFAULT NULL COMMENT '应用名称',
                        `app_desc` text DEFAULT NULL COMMENT '应用描述',
                        `app_doc` text DEFAULT NULL COMMENT '应用需求文档',
                        `preview_url` varchar(100) DEFAULT NULL COMMENT '应用预览url',
-                       `app_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '应用类型：0=html，1=vue3，2=vue3_spring',
+                       `app_type` tinyint(1) DEFAULT '0' COMMENT '应用类型：0=html，1=vue3，2=vue3_spring',
                        `deploy` tinyint DEFAULT 0,
                        PRIMARY KEY (`id`),
                        KEY `idx_apps_user` (`user_id`)
